5c4ded91c3be6e47dc5822b7a2be79b928ca312b
jaisherma <79637980+jaisherma@users.noreply.github.com>
2023-09-25 01:43:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is my Python web application.'
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
